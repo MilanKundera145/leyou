41041ba6a6597e53db797269e5090880b1f51065
Mask <645561079@qq.com>
2019-08-03 17:20:12 +0800

    图片上传

--- leyou-upload/src/main/java/com/leyou/upload/service/impl/UploadServiceImpl.java
+++ leyou-upload/src/main/java/com/leyou/upload/service/impl/UploadServiceImpl.java
@@ -1,7 +1,13 @@
 package com.leyou.upload.service.impl;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.upload.service.UploadService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,7 +22,45 @@ import java.util.List;
 @Service("upload")
 public class UploadServiceImpl implements UploadService {
 
-    private static final List<String> CONTENT_TYPES = Arrays.asList("image/jpeg","image/gif");
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    private static final List<String> CONTENT_TYPES = Arrays.asList("image/jpeg", "image/gif");
+
+    public String upload(MultipartFile file) {
+
+        String originalFilename = file.getOriginalFilename();
+        // 校验文件的类型
+        String contentType = file.getContentType();
+        if (!CONTENT_TYPES.contains(contentType)){
+            // 文件类型不合法，直接返回null
+            log.info("文件类型不合法：{}", originalFilename);
+            return null;
+        }
+
+        try {
+            // 校验文件的内容
+            BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
+            if (bufferedImage == null){
+                log.info("文件内容不合法：{}", originalFilename);
+                return null;
+            }
+
+            // 保存到服务器
+            // file.transferTo(new File("C:\\leyou\\images\\" + originalFilename));
+            String ext = StringUtils.substringAfterLast(originalFilename, ".");
+            StorePath storePath = this.storageClient.uploadFile(file.getInputStream(), file.getSize(), ext, null);
+
+            // 生成url地址，返回
+            return "http://image.leyou.com/" + storePath.getFullPath();
+        } catch (IOException e) {
+            log.info("服务器内部错误：{}", originalFilename);
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /*private static final List<String> CONTENT_TYPES = Arrays.asList("image/jpeg","image/gif");
 
     @Override
     public String upload(MultipartFile file) {
@@ -39,12 +83,12 @@ public class UploadServiceImpl implements UploadService {
             }
 
             // 保存到服务器
-            file.transferTo(new File("C:\\Users\\王刚\\Pictures\\images\\"+originalFilename));
+            file.transferTo(new File("D:\\IDEA\\images\\"+originalFilename));
             // 生成url地址，返回
             return "http://image.leyou.com/"+originalFilename;
         } catch (IOException e) {
             log.error("服务器内部错误：{}",e);
         }
         return null;
-    }
+    }*/
 }
